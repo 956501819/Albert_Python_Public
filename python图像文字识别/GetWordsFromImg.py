dd33125759bf802de6fa0cb68eb7fd61d35da78f
956501819 <ly956501819@foxmail.com>
2020-01-06 15:02:36 +0800

    python
    python

--- python图像文字识别/GetWordsFromImg.py
+++ python图像文字识别/GetWordsFromImg.py
@@ -1,6 +1,7 @@
 # -*- coding :utf-8 -*-
 #Author :刘悦
-#前提是安装识别引擎tesseract-ocr
+#前提是安装识别引擎tesseract-ocr，是一个软件
+#和pytesseract，pip install pytesseract，可以命令安装
 from PIL import Image
 import pytesseract
 #上面都是导包，只需要下面这一行就能实现图片文字识别
